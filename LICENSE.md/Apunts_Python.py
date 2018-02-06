94955d03fb00b874910552f61993c4b98332e843
Alejandro <36173367+ThePapagallo@users.noreply.github.com>
2018-02-06 11:57:58 +0100

    Update Apunts_Python.py

--- LICENSE.md/Apunts_Python.py
+++ LICENSE.md/Apunts_Python.py
@@ -1,3 +1,7 @@
-# coding: utf­8
-edad = 16
-print "Tu edad es: " , edad , " años"
+
+
+edad = input("Indique su edad: ")
+if edad >= 18 :
+    print "Eres mayor de edad. Puedes pasar!!!"
+else:
+    print "Lo siento con " , edad , " no puedes pasar"
