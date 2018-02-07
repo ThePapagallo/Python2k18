aeadba866338b6e0cccc389af7a0ceefa255678d
Alejandro <36173367+ThePapagallo@users.noreply.github.com>
2018-02-07 23:31:30 +0100

    Update and rename Apunts_Python.py to ejercicio-edad-1.py

--- LICENSE.md/Apunts_Python.py
+++
@@ -1,7 +0,0 @@
-
-
-edad = input("Indique su edad: ")
-if edad >= 18 :
-    print "Eres mayor de edad. Puedes pasar!!!"
-else:
-    print "Lo siento con " , edad , " no puedes pasar"
---
+++ LICENSE.md/ejercicio-edad-1.py
@@ -0,0 +1,3 @@
+#coding utf-8
+#Alejandro Gambín Gómez 7/2/2018
+edad=input("Que edad tienes?")
