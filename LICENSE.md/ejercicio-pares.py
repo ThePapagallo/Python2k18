37e4a93e8e6db12683a74401edfde7d869355fe3
Alejandro <36173367+ThePapagallo@users.noreply.github.com>
2018-02-08 21:48:35 +0100

    Create  ejercicio-pares.py

---
+++ LICENSE.md/ejercicio-pares.py
@@ -0,0 +1,10 @@
+#coding: utf8
+#Alejandro Gambín Gómez 08/02/2017
+
+numero=input("Introduzca número")
+	 
+if (numero %2 ==0):
+	print "Qué bonito número par"
+else:
+    print "Que número más vulgar"
+   	 
