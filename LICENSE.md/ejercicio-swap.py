6c05597ba2aae72dcaabf685794f60345ce50396
Alejandro <36173367+ThePapagallo@users.noreply.github.com>
2018-02-08 22:03:51 +0100

    Create ejercicio-swap.py

---
+++ LICENSE.md/ejercicio-swap.py
@@ -0,0 +1,13 @@
+#coding: utf8
+#Alejandro Gambín Gómez
+
+mano_der= "movil"
+mano_izq= "bocadillo"
+
+mano_extra= mano_der
+mano_der= mano_izq
+mano_izq= mano_extra
+
+print mano_der
+print mano_izq
+print mano_extra
