9a65d5bd60308e4483731f1cd959c8aed6ffde90
Alejandro <36173367+ThePapagallo@users.noreply.github.com>
2018-02-14 23:04:24 +0100

    Create ejercicio-mayor.py

---
+++ LICENSE.md/ejercicio-mayor.py
@@ -0,0 +1,14 @@
+#coding: utf8
+#Alejandro Gambín
+
+Valor1=input("Introducir número de valor1 ")
+Valor2=input("Introducir número de valor2 ")
+
+if Valor1 > Valor2:
+    print "Valor_1 wins"
+else:
+    if Valor1 == Valor2:
+   	 print "Draw"
+   	 
+    else:
+   	 print "Valor_2 wins"
