5890dacbbb9ac6881cf49cb094384973c912fb63
Alejandro <36173367+ThePapagallo@users.noreply.github.com>
2018-03-19 23:30:06 +0100

    Update ejercicio-bucle-sumar1.py

--- LICENSE/ejercicio-bucle-sumar1.py
+++ LICENSE/ejercicio-bucle-sumar1.py
@@ -5,14 +5,11 @@ cifra= 1
 maximo= 5
 suma= 0
 while ( salir=="N"):
-	if(cifra%2==0):
-    	print (cifra ,)
-    	if(cifra<=maximo-2):
-        	print ("+" ,)
-       	 
-    	suma=suma+cifra
-   	 
+	print(cifra)
+    
+    
+	suma=suma+cifra
 	cifra=cifra+1
-  if( cifra > maximo):
+	if( cifra > maximo):
         	salir= "S"
 print ("=" , suma)
